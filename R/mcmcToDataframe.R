b7c7c30818c552829b7b5cad5c0b99ccdc335829
Doug Leasure <douglas.leasure@demography.ox.ac.uk>
2024-10-21 11:29:50 +0100

    cleanup repo for publication

--- R/mcmcToDataframe.R
+++
@@ -1,9 +0,0 @@
-# convert mcmc list to data.frame 
-mcmcToDataframe <- function(mcmc_list){
-  d <- mcmc_list[[1]]
-  for(i in 2:length(mcmc_list)){
-    d <- rbind(d, mcmc_list[[i]])
-  }
-  d <- data.frame(d)
-  return(d)
-}
